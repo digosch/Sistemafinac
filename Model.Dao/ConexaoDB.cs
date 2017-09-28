7a04a982b4ea9d2ee10b6fa9f9204865b2f5fd88
Rodrigo Schmidt <rodrigo_7sch@hotmail.com>
2017-09-28 19:26:41 -0300

    Add project files.

---
+++ Model.Dao/ConexaoDB.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.SqlClient;
+
+namespace Model.Dao
+{
+    public class ConexaoDB
+    {
+        private static ConexaoDB objConexaoDB = null;
+        private SqlConnection con;
+
+        private ConexaoDB()
+        {
+            con = new SqlConnection("Data Source=HUGO-PC\\HUGO; Initial Catalog=financeiro; Integrated Security=True");
+        }
+
+        public static ConexaoDB saberEstado()
+        {
+            if (objConexaoDB == null)
+            {
+                objConexaoDB = new ConexaoDB();
+            }
+
+            return objConexaoDB;
+        }
+
+
+        public SqlConnection getCon()
+        {
+            return con;
+        }
+
+        public void CloseDB()
+        {
+            objConexaoDB = null;
+        }
+    }
+}
